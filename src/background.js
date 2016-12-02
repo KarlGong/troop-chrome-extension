2297a88321e03a9e3cafe53f225232b5e9be8e13
Karl Gong <karl.gong@outlook.com>
2016-12-02 14:27:39 +0800

    temp angular

---
+++ src/background.js
@@ -0,0 +1,47 @@
+chrome.tabs.getCurrent(function (currentTab) {
+    alert(currentTab);
+    chrome.debugger.attach({ //debug at current tab
+        tabId: currentTab.id
+    }, "1.0", function () {
+        chrome.debugger.sendCommand({ //first enable the Network
+            tabId: currentTab.id
+        }, "Network.enable");
+    });
+});
+
+
+
+//
+// chrome.tabs.query({
+//         currentWindow: true,
+//         active: true
+//     },
+//     function (tabArray) {
+//         currentTab = tabArray[0];
+//         chrome.debugger.attach({ //debug at current tab
+//             tabId: currentTab.id
+//         }, "1.0", function () {
+//             chrome.debugger.sendCommand({ //first enable the Network
+//                 tabId: currentTab.id
+//             }, "Network.enable");
+//         });
+//     }
+// );
+//
+// chrome.debugger.onEvent.addListener(function (debuggeeId, message, params) {
+//     if (currentTab.id != debuggeeId.tabId) {
+//         return;
+//     }
+//
+//     if (message == "Network.responseReceived") { //response return
+//         chrome.debugger.sendCommand({
+//             tabId: debuggeeId.tabId
+//         }, "Network.getResponseBody", {
+//             "requestId": params.requestId
+//         }, function (response) {
+//
+//             chrome.debugger.detach(debuggeeId);
+//         });
+//     }
+//
+// });
--- src/troop.js
+++ src/troop.js
@@ -1,19 +1,40 @@
-$(function () {
-    $("table").resizableColumns();
-    // $("table").resizableColumns({
-    //     store: window.localStorage
-    // });
+angular.module("troop", [])
+    .controller("troopController", function ($scope) {
+        var troopServicePattern = /^.+?queryproxy.*?$/;
 
+        $scope.requests = [];
 
-    chrome.devtools.network.onRequestFinished.addListener(function (request) {
-            if (request.request.url.indexOf("services/shared/queryproxy") >= 0) {
-                $("#a").html("abc");
-            }
-        }
-    );
+        var currentTab;
 
+        
 
+        chrome.devtools.network.onRequestFinished.addListener(
+            function (entry) {
+                if (entry.request.url.match(troopServicePattern)) {
+                    var params = unescape(entry.request.postData.params[0].value).split("|");
+                    for (var i = 0; i < params.length; i++) {
+                        var splitterIndex = params[i].indexOf("!");
+                        var troopCommand = params[i].substring(0, splitterIndex);
+                        var troopParam = params[i].substring(splitterIndex + 1);
+                        if (troopParam.charAt(0) === "'" && troopParam.charAt(troopParam.length - 1) === "'") {
+                            troopParam = troopParam.substring(1, params[i].length - 2);
+                        }
 
-});
+                        $scope.requests.push({
+                            troop: {
+                                command: troopCommand,
+                                param: troopParam,
+                            },
+                            request: entry.request,
+                            response: entry.response
+                        });
+
+                    }
+
+                }
+            });
+
+    });
+
 
 
