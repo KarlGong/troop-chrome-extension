cadda810dfd8fdbbba3583feb20202fe3dde3658
Karl Gong <karl.gong@outlook.com>
2016-12-02 19:06:13 +0800

    background

--- src/background.js
+++ src/background.js
@@ -1,51 +1,22 @@
-// chrome.tabs.getCurrent(function (currentTab) {
-//     alert(currentTab);
-//     chrome.debugger.attach({ //debug at current tab
-//         tabId: currentTab.id
-//     }, "1.0", function () {
-//         chrome.debugger.sendCommand({ //first enable the Network
-//             tabId: currentTab.id
-//         }, "Network.enable");
-//     });
-// });
-
-
 chrome.tabs.onActivated.addListener(function (activeInfo) {
+    chrome.debugger.attach({ //debug at current tab
+        tabId: activeInfo.tabId
+    }, "1.0", function () {
+        chrome.debugger.sendCommand({ //first enable the Network
+            tabId: activeInfo.tabId
+        }, "Network.enable");
 
+        chrome.debugger.onEvent.addListener(function (debuggeeId, message, params) {
+            if (activeInfo.tabId === debuggeeId.tabId && message === "Network.responseReceived") { //response return
+                chrome.debugger.sendCommand({
+                    tabId: debuggeeId.tabId
+                }, "Network.getResponseBody", {
+                    "requestId": params.requestId
+                }, function (response) {
+                    alert(response);
+                    chrome.debugger.detach(debuggeeId);
+                });
+            }
+        });
+    });
 });
-
-
-//
-// chrome.tabs.query({
-//         currentWindow: true,
-//         active: true
-//     },
-//     function (tabArray) {
-//         currentTab = tabArray[0];
-//         chrome.debugger.attach({ //debug at current tab
-//             tabId: currentTab.id
-//         }, "1.0", function () {
-//             chrome.debugger.sendCommand({ //first enable the Network
-//                 tabId: currentTab.id
-//             }, "Network.enable");
-//         });
-//     }
-// );
-//
-// chrome.debugger.onEvent.addListener(function (debuggeeId, message, params) {
-//     if (currentTab.id != debuggeeId.tabId) {
-//         return;
-//     }
-//
-//     if (message == "Network.responseReceived") { //response return
-//         chrome.debugger.sendCommand({
-//             tabId: debuggeeId.tabId
-//         }, "Network.getResponseBody", {
-//             "requestId": params.requestId
-//         }, function (response) {
-//
-//             chrome.debugger.detach(debuggeeId);
-//         });
-//     }
-//
-// });
